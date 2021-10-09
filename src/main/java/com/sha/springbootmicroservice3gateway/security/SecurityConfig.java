d7148052add0fd95153b491002654fc4d68812e4
Satyapal Singh <satyasingh100@gmail.com>
2021-10-09 16:39:52 +0530

    xyz

--- src/main/java/com/sha/springbootmicroservice3gateway/security/SecurityConfig.java
+++ src/main/java/com/sha/springbootmicroservice3gateway/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
         http.authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .antMatchers("/api/authentication/**").permitAll()//login and register pre-path
-				.antMatchers("/", "/csrf", "/v2/api-docs", "/swagger-resources/**", "/configuration/ui", "/swagger-resources", "/swagger-resources/configuration/security", "/configuration/security", "/swagger-ui.html", "/webjars/**", "/swagger-ui/**").permitAll()
+				.antMatchers("/", "/csrf", "/v3/api-docs", "/swagger-resources/**", "/configuration/ui", "/swagger-resources", "/swagger-resources/configuration/security", "/configuration/security", "/swagger-ui.html", "/webjars/**", "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
