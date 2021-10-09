1f81036820b3cde9e26c0bc9476cbfd783a82cd8
Satyapal Singh <satyasingh100@gmail.com>
2021-10-09 17:03:01 +0530

    xyz

--- src/main/java/com/sha/springbootmicroservice3gateway/SpringBootMicroservice3GatewayApplication.java
+++ src/main/java/com/sha/springbootmicroservice3gateway/SpringBootMicroservice3GatewayApplication.java
@@ -7,8 +7,12 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @PropertySource("classpath:application-${spring.profiles.active:default}.properties")
+@OpenAPIDefinition(info = @Info(title = "Gateway API", version = "3.0.1", description = "Gateway APIs"))
 public class SpringBootMicroservice3GatewayApplication {
 
 	public static void main(String[] args) {
